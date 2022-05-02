5a98e9c439f5e18279f41f1ac2e720461a6b264a
Cahcastroc <camila.castronunes@gmail.com>
2022-05-02 18:13:11 -0300

    Criação da ResultActivity

---
+++ app/src/main/java/me/camila/projetofabricasinapse/ResultActvity.kt
@@ -0,0 +1,11 @@
+package me.camila.projetofabricasinapse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_actvity)
+    }
+}
